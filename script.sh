f83800f039b621db224502be7b42844c2b140358
Manish Jesani <40167526+manijesani@users.noreply.github.com>
2021-03-26 17:30:31 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#for local system
+ip=`ifconfig | grep "inet 192" | grep "broadcast" | awk '{print $2}'`
+
+#for public system
+#ip=`curl icanhazip.com`
+
+#Entry For Settings.py line 28
+sed "s/host_ip/'$ip'/g" todolist/settings.py
