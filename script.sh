803be92fbacd0297a37a4f2a70395fa6b1b24a45
Manish Jesani <40167526+manijesani@users.noreply.github.com>
2021-03-31 12:41:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #for local system
-ip=`ifconfig | grep "inet 192" | grep "broadcast" | awk '{print $2}'`
+ip=`hostname -I | awk '{print $1}'`
 
 #for public system
 #ip=`curl icanhazip.com`
 
 #Entry For Settings.py line 28
-sed "s/host_ip/$ip/g" todolist/settings.py > todolist/settings.py
+sed "s/host_ip/$ip/g" ./todolist/settings.py
