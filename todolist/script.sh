4a369382f5698aa73f6d91984448aea46cde4619
Manish Jesani <40167526+manijesani@users.noreply.github.com>
2021-03-31 13:04:37 +0530

    Create script.sh

---
+++ todolist/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#for local system
+ip=`hostname -I | awk '{print $1}'`
+
+#for public system
+#ip=`curl icanhazip.com`
+
+#Entry For Settings.py line 28
+sed "s/host_ip/$ip/g" .//settings.py > ./settings.py
